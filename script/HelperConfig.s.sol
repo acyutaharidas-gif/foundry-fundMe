5a30e036428ac509e09ee26bb95d3a92035a05e3
acyutaharidas-gif <acyutaharidas@gmail.com>
2026-02-21 12:31:13 +0000

    refactor code

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
+
+contract HelperConfig is Script {
+    NetworkConfig public activeNetworkConfig;
+
+    struct NetworkConfig {
+        address priceFeed;
+    }
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else {
+            activeNetworkConfig = getAnvilEthConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        return NetworkConfig({priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306});
+    }
+
+    function getAnvilEthConfig() public returns (NetworkConfig memory) {
+        // price feed address
+
+        // 1.deploy
+        // 2. return mock address
+
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(8, 2000e8);
+        vm.stopBroadcast();
+
+        return NetworkConfig({priceFeed: address(mockPriceFeed)});
+    }
+}
--- src/FundMe.sol
+++ src/FundMe.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+// Note: The AggregatorV3Interface might be at a different location than what was in the video!
+import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 import {PriceConverter} from "./PriceConverter.sol";
 
 error FundMe__NotOwner();
@@ -8,22 +10,35 @@ error FundMe__NotOwner();
 contract FundMe {
     using PriceConverter for uint256;
 
-    uint256 public constant MINIMUM_USD = 1e18; //constant takes less gas
-
+    mapping(address => uint256) public addressToAmountFunded;
     address[] public funders;
-    mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
 
-    address public immutable i_owner;
+    // Could we make this constant?  /* hint: no! We should make it immutable! */
+    address public /* immutable */ i_owner;
+    uint256 public constant MINIMUM_USD = 5 * 10 ** 18;
+    AggregatorV3Interface private s_priceFeed;
 
-    constructor() {
+    constructor(address priceFeed) {
         i_owner = msg.sender;
+        s_priceFeed = AggregatorV3Interface(priceFeed);
     }
 
     function fund() public payable {
-        require(msg.value.getConversionRate() >= MINIMUM_USD, "didn't send enough ETH");
-        // 1e18 = 1 ETH = 1 * 10 ** 18
-        funders.push(msg.sender);
+        require(msg.value.getConversionRate(s_priceFeed) >= MINIMUM_USD, "You need to spend more ETH!");
+        // require(PriceConverter.getConversionRate(msg.value) >= MINIMUM_USD, "You need to spend more ETH!");
         addressToAmountFunded[msg.sender] += msg.value;
+        funders.push(msg.sender);
+    }
+
+    function getVersion() public view returns (uint256) {
+        // AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return s_priceFeed.version();
+    }
+
+    modifier onlyOwner() {
+        // require(msg.sender == owner);
+        if (msg.sender != i_owner) revert FundMe__NotOwner();
+        _;
     }
 
     function withdraw() public onlyOwner {
@@ -31,15 +46,11 @@ contract FundMe {
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
         }
-
-        // reset the array
         funders = new address[](0);
-        // withdraw funds
-
-        // transfer (deprecated)
+        // // transfer
         // payable(msg.sender).transfer(address(this).balance);
 
-        // send (deprecated)
+        // // send
         // bool sendSuccess = payable(msg.sender).send(address(this).balance);
         // require(sendSuccess, "Send failed");
 
@@ -47,18 +58,6 @@ contract FundMe {
         (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
         require(callSuccess, "Call failed");
     }
-
-    modifier onlyOwner() {
-        // require(msg.sender == i_owner, "Must be owner!");
-        if (msg.sender != i_owner) {
-            revert FundMe__NotOwner();
-        }
-        _; //execute the rest of the code
-    }
-
-    // receive() special fn that gets triggered when ETH is sent w/o any function calling [NO data must be sent]
-    // fallback() another sp. fn. that gets triggered when NO valid function is found [data is ok/optional]
-
     // Explainer from: https://solidity-by-example.org/fallback/
     // Ether is sent to contract
     //      is msg.data empty?
@@ -71,11 +70,20 @@ contract FundMe {
     //  /        \
     //receive()  fallback()
 
-    receive() external payable {
-        fund();
-    }
-
     fallback() external payable {
         fund();
     }
+
+    receive() external payable {
+        fund();
+    }
 }
+
+// Concepts we didn't cover yet (will cover in later sections)
+// 1. Enum
+// 2. Events
+// 3. Try / Catch
+// 4. Function Selector
+// 5. abi.encode / decode
+// 6. Hash with keccak256
+// 7. Yul / Assembly
